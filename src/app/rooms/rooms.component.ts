251977163aaa376752dbf252501bd380b3f5d550
SamyakPaharia <9822249355@Papa>
2024-05-14 17:35:26 +0530

    Learned: Structural and Attribute Directives Structural Directives: Can add or remove elements from the DOM. They are preceded by an asterisk (*). Attribute Directives: Change the appearance or behavior of an element, component, or another directive.

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,28 +1,56 @@
 import { Component } from '@angular/core';
+import { Rooms,RoomsList } from './rooms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'hinv-rooms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
 export class RoomsComponent {
 
   hotelName = 'Hilton';
-  numberOfRooms = 10;
-  flag = false;
+  numberOfRooms = 13;
+  hideRooms: boolean = false;
+  role: string = 'userssss';
 
-  increment() {
-    this.numberOfRooms++;
-  }
+  rooms: Rooms = {
+    availableRooms: 10,
+    bookedRooms: 5,
+    totalRooms: 20,
+  };
 
-  decrement() {
-    this.numberOfRooms--;
+  roomList: RoomsList[] = [{
+    type: 'Single',
+    checkin: new Date(),
+    checkout: new Date(),
+    ammenties: 'TV, AC, Wifi',
+    price: 200,
+    location: 'Bangalore'
+  },
+  {
+    type: 'Double',
+    checkin: new Date(),
+    checkout: new Date(),
+    ammenties: 'TV, AC, Wifi',
+    price: 100,
+    location: 'Bangalore'
+  },
+
+  {
+    type:'Suite',
+    checkin: new Date(),
+    checkout: new Date(),
+    ammenties: 'TV, AC, Wifi',
+    price: 400,
+    location: 'Bangalore'
   }
+]
 
   toggle() {
-    this.flag = !this.flag;
+    this.hideRooms = !this.hideRooms;
   }
 
 }
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,18 @@
+export interface Rooms {
+
+    availableRooms: number;
+    bookedRooms: number;
+    totalRooms: number;
+
+
+}
+
+export interface RoomsList
+{
+    type: string;
+    checkin:Date;
+    checkout:Date;
+    ammenties: string;
+    price: number;
+    location: string;
+}
